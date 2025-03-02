beef52435b6dcb0104033b0b7b80a3a1c0c32b58
RhoDShuXue <160774879+RhoDShuXue@users.noreply.github.com>
2025-03-02 11:00:39 -0500

    Update checkForFileCreated.py

--- New/checkForFileCreated.py
+++ New/checkForFileCreated.py
@@ -2,13 +2,15 @@ from watchdog import *
 import time
 import subprocess
 
+
+
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-# Define the folder to monitor
+
+
 FOLDER_TO_WATCH = "D:/vsCode/python/New/PDFInputs"  # Replace with the path to your folder
 
-# Custom event handler
 class FileCreatedHandler(FileSystemEventHandler):
     def on_created(self, event):
         """
@@ -21,12 +23,10 @@ class FileCreatedHandler(FileSystemEventHandler):
         if not event.is_directory:  # Ignore directory creation events
             print(f"New file created: {event.src_path}")
 
-# Set up the observer
 event_handler = FileCreatedHandler()
 observer = Observer()
 observer.schedule(event_handler, path=FOLDER_TO_WATCH, recursive=False)
 
-# Start monitoring
 print(f"Monitoring folder: {FOLDER_TO_WATCH}")
 observer.start()
 
